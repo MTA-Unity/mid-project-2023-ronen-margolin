67cc8802819374bb2dc8239e19e03738e5ff51ae
Ronen Margolin <roni948576@gmail.com>
2023-08-23 13:02:58 +0300

    blocks appear well

--- Assets/Scripts/BlockPoolManager.cs
+++ Assets/Scripts/BlockPoolManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Rendering;
 
 public class BlockPoolManager : MonoBehaviour
 {
@@ -21,10 +22,12 @@ public class BlockPoolManager : MonoBehaviour
     List<bool> alive = new List<bool>();
 
     private void Awake() {
-        leftBorder = cam.ScreenToWorldPoint(new Vector3(0,0,0)).x;
-        topBorder = cam.ScreenToWorldPoint(new Vector3(0,Screen.height,0)).y;
-        bottomBorder = cam.ScreenToWorldPoint(new Vector3(0,Screen.height/2,0)).y;
-        rightBorder = cam.ScreenToWorldPoint(new Vector3(Screen.width,0,0)).x;
+        leftBorder = cam.ScreenToWorldPoint(new Vector3(Screen.safeArea.xMin,0,0)).x;
+        topBorder = cam.ScreenToWorldPoint(new Vector3(0,Screen.safeArea.yMax,0)).y;
+        bottomBorder = cam.ScreenToWorldPoint(new Vector3(0,Screen.safeArea.center.y,0)).y;
+        rightBorder = cam.ScreenToWorldPoint(new Vector3(Screen.safeArea.xMax,0,0)).x;
+        colWidth = (rightBorder - leftBorder)/blocksInRow;
+        rowWidth = (topBorder - bottomBorder)/blocksInCol;
     }
 
     void Start()
@@ -36,12 +39,38 @@ public class BlockPoolManager : MonoBehaviour
     private List<Vector2> calculateBlocks()
     {
         List<Vector2> result = new List<Vector2>(pool.amountToPool);
+        for(int i=0; i<blocksInCol; i++)
+        {
+            for(int j=0;j<blocksInRow;j++)
+            {
+                result.Add(new Vector2(
+                    colWidth/2+ leftBorder + j*colWidth,
+                    topBorder - rowWidth/2 - i*rowWidth
+                ));
+                alive.Add(false);
+            }
+        }
         return result;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        for(int i=0;i<pool.amountToPool;i++)
+        {
+            drawBlock(i);
+        }
+    }
+
+    void drawBlock(int i)
+    {
+        if(!alive[i])
+        {
+            GameObject block = pool.GetPooledObject();
+            block.SetActive(true);
+            block.transform.position = blockPositions[i];
+            block.transform.localScale = new Vector3(colWidth*0.7f, rowWidth*0.7f, 1);
+            alive[i] = true;
+        }
     }
 }
