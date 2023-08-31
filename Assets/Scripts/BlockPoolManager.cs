3c13eeaeb803693973eaae7422adfe58bff71466
Ronen Margolin <roni948576@gmail.com>
2023-08-31 22:17:34 +0300

    build is working correctly

--- Assets/Scripts/BlockPoolManager.cs
+++ Assets/Scripts/BlockPoolManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.Rendering;
 
 public class BlockPoolManager : MonoBehaviour
 {
